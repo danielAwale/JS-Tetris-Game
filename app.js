6dde176d7381dfeabad0e37caa3bd2a67609e266
danielAwale <danielmadhukarawale@gmail.com>
2022-11-06 08:46:34 -0800

    work on the app.js file, assigning variables, creating arrays

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const squareWidth = 10
 
+  console.log(squares);
+  console.log(grid);
 })
