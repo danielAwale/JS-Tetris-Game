285dbd864807c9c6383bd244a7311393f8a95212
danielAwale <danielmadhukarawale@gmail.com>
2022-11-06 09:02:41 -0800

    contain all the variables

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   let squares = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.querySelector('#score')
+  const startButton = document.querySelector('#dtart-button')
   const squareWidth = 10
 
-  console.log(squares);
-  console.log(grid);
 })
